c6785e198a87b2f105f01a056cc24d64ec2b5391
MengYangkov <phengmengheak@outlook.com>
2025-06-07 16:36:06 +0700

    Render dynamic categories and add sass style

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
+import './categories.styles.scss';
 
-const App = () =>{
-  return (
+const App = () => {
+  const categories = [
+    { id: 1, title: 'Hats' },
+    { id: 2, title: 'Jackets' },
+    { id: 3, title: 'Sneakers' },
+    { id: 4, title: 'Mens' },
+    { id: 5, title: 'Womens' },
+  ];
+
+  return (  
     <div className="categories-container">
-      <div className="catesgory-container">
-      {/* <img src=""/> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>  
-      </div>
-      <div className="catesgory-container">
-      {/* <img src=""/> */}
-        <div className="category-body-container"> 
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>  
-      </div>
-      <div className="catesgory-container">
-      {/* <img src=""/> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>  
-      </div>
-      <div className="catesgory-container">
-      {/* <img src=""/> */}
-        <div className="category-body-container">
-          <h2>Man</h2>
-          <p>Shop Now</p>
-        </div>  
-      </div>
-      <div className="catesgory-container">
-      {/* <img src=""/> */}
-        <div className="category-body-container">
-          <h2>Women</h2>
-          <p>Shop Now</p>
-        </div>  
-      </div>
+      {categories.map(({title,id}) => (
+        <div key={id} className="category-container">
+          <div className="background-image"/>
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
