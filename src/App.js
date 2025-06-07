810b1e5974583a39f75a16e29a0e62bd6772ef15
MengYangkov <phengmengheak@outlook.com>
2025-06-07 17:09:53 +0700

    break it into in own component to reusable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
+import CategoryItem from '../src/components/category-items/category-item.component.jsx';
+import categories from '../src/components/categories-main/categories.json';
 import './categories.styles.scss';
 
 const App = () => {
-  const categories = [
-    { id: 1, title: 'Hats' },
-    { id: 2, title: 'Jackets' },
-    { id: 3, title: 'Sneakers' },
-    { id: 4, title: 'Mens' },
-    { id: 5, title: 'Womens' },
-  ];
 
   return (  
     <div className="categories-container">
-      {categories.map(({title,id}) => (
-        <div key={id} className="category-container">
-          <div className="background-image"/>
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key= {category.id} category = {category}/>
       ))}
     </div>
   );
